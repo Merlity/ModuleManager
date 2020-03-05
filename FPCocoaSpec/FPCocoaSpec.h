175c31bef091341866da950d42cb058856a91f91
Merlity <623427175@qq.com>
2020-03-05 15:40:05 +0800

    add pod file

---
+++ FPCocoaSpec/FPCocoaSpec.h
@@ -0,0 +1,16 @@
+//
+//  FPCocoaSpec.h
+//  
+//
+//  Created by 易椰城 on 2020/3/5.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FPCocoaSpec : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FPCocoaSpec/FPCocoaSpec.m
@@ -0,0 +1,24 @@
+//
+//  FPCocoaSpec.m
+//  
+//
+//  Created by 易椰城 on 2020/3/5.
+//
+
+#import "FPCocoaSpec.h"
+
+@interface FPCocoaSpec ()
+
+@end
+
+@implementation FPCocoaSpec
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
